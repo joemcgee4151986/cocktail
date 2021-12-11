60798571631ca803a3a66860780082f51c12e311
joe mcgee <jrmcgee@asu.edu>
2021-12-11 20:40:49 +0900

    did some redesigns

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector('button').addEventListener('click', getDrink)
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getDrink(){
+  let drinkChoice = document.querySelector('input').value
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+drinkChoice
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        console.log(data.drinks[0])
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+
       })
       .catch(err => {
           console.log(`error ${err}`)
