c471a88e8b092baebd91de06c7e62be97f1586d7
joe mcgee <jrmcgee@asu.edu>
2022-02-17 01:33:53 +0900

    added more drinks to the search bar

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function getDrink(){
       .then(data => {
         console.log(data.drinks[0])
         document.querySelector('h2').innerText = data.drinks[0].strDrink
+       
         document.querySelector('img').src = data.drinks[0].strDrinkThumb
         document.querySelector('h3').innerText = data.drinks[0].strInstructions
 
